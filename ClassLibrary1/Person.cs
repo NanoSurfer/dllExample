5626f87a120d88d8e6e9abda8fdc4dd026114149
NanoSurfer <shawnbrewis@gmail.com>
2013-06-07 14:06:50 +0200

    First commit

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        private string idNumber;
+
+        public string IdNumber
+        {
+            get { return idNumber; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        // Constructor with zero values
+        public Person()
+        {
+            idNumber = "";
+            lastName = "unknown";
+            firstName = string.Empty;
+            age = 0;
+        }
+
+        // Constructor with four arguments
+        public Person(string id, string lname, string fname, int anAge)
+        {
+            idNumber = id;
+            lname = lastName;
+            fname = firstName;
+            anAge = age;
+        }
+
+        // Constructor with 3 arguments
+        public Person(string id, string lname, string fname)
+        {
+            idNumber = id;
+            lname = lastName;
+            fname = firstName;
+        }
+
+        // Overrides toString()
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public virtual int GetSleepAmt()
+        {
+            return 8;
+        }
+
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary1;
+
+namespace Student
+{
+    public class Student : Person
+    {
+        private string major;
+
+        public string Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+        private int studentId;
+
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+        public Student()
+            : base()
+        {
+            major = "unknown";
+            studentId = 0;
+        }
+
+        // Constructor that sends arguments to the base class
+        public Student(string id, string fname, string lname, string maj, int sId)
+            : base(id, lname, fname)
+        {
+            major = maj;
+            studentId = sId;
+        }
+
+        public override int GetSleepAmt()
+        {
+            return 6;
+        }
+
+        // Using the base keyword calls the overridden 
+        // GetSleepAmt() method of the Person class
+        public int CallOverriddenGetSleepAmt()
+        {
+            return base.GetSleepAmt();
+        }
+    }
+}
